8c6a91aae776cde22875daafb8b5c18cd5f5ca81
MirErrorCode <MirErrorCode@gmail.com>
2025-07-19 15:23:18 +0300

    day-5 Screen Details (finish)

--- app/src/main/java/mir/errorcode/moviesapp/screens/DetailsScreen.kt
+++ app/src/main/java/mir/errorcode/moviesapp/screens/DetailsScreen.kt
@@ -1,16 +1,91 @@
 package mir.errorcode.moviesapp.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import mir.errorcode.moviesapp.MainViewModel
+import mir.errorcode.moviesapp.utils.HtmlText
 
 
 @Composable
-fun DetailsScreen(navController: NavController, viewModel: MainViewModel, itemId: String) {
+fun DetailsScreen(viewModel: MainViewModel, itemId: String) {
 
-    Text("Details screen: item id: ${itemId}")
+    val currentItem = viewModel.allMovies
+        .observeAsState(listOf()).value
+        .firstOrNull { it.id == itemId.toInt() }
 
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 24.dp, horizontal = 8.dp)
+    ) {
+        LazyColumn {
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(currentItem?.image?.medium),
+                        contentDescription = "image",
+                        modifier = Modifier.size(512.dp)
+                    )
+                    Text(text = currentItem?.name ?: "",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 32.sp,
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
 
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        Text(
+                            text = "Rating: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp
+                        )
+                        Text(
+                            text = currentItem?.rating?.average.toString(),
+                            fontSize = 18.sp
+                        )
+                    }
+
+                    Row(
+                        modifier = Modifier.padding(top = 8.dp)
+                    ) {
+                        Text(
+                            text = "Genre: ",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 18.sp,
+                            modifier = Modifier.padding(bottom = 10.dp)
+                        )
+                        currentItem?.genres?.take(2)?.forEach {
+                            Text(
+                                text = " [$it] ",
+                                fontSize = 18.sp
+                            )
+                        }
+                    }
+                    HtmlText(
+                        html = currentItem?.summary ?: ""
+                    )
+                }
+            }
+        }
+    }
 
 }
